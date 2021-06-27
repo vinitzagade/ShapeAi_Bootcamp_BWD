f51b6fc90009086999c4bb5950a1905702efdf81
vinitzagade <54323557+vinitzagade@users.noreply.github.com>
2021-06-27 22:53:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+
+function App() {
+
+  return (
+
+    <div>
+    <Header />
+    <Info />
+    <Info />
+    <Info />
+    <Info />
+    <Info />
+    <Footer />
+    </div>
+    
+  
+  );
+}
+
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+
+  return (
+
+    <div className="note">
+    <h1>Javascript and React JS</h1>
+    <p>A basic Web development  React Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info
